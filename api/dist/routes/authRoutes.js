d25b0b3270de792d88b80f361f134ccada820f65
Yutaro Negi <souza_yutaro@hotmail.com>
2023-03-03 16:44:17 -0300

    feat: added login limits

--- api/dist/routes/authRoutes.js
+++ api/dist/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { AuthController } from '../controllers/AuthController.js';
+import rateLimit from 'express-rate-limit';
 const router = express.Router();
 dotenv.config();
-router.post('/login', AuthController.login);
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: "Muitas tentativas de login. Tente novamente mais tarde."
+});
+router.post('/login', limiter, AuthController.login);
 router.post('/register', AuthController.register);
 router.post('/forgot-password', AuthController.forgotPassword);
 export { router as authRoutes };
--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv'
 import { AuthController } from '../controllers/AuthController.js';
+import rateLimit from 'express-rate-limit';
 const router = express.Router()
 dotenv.config()
 
-router.post('/login', AuthController.login)
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 5, // número máximo de solicitações por IP
+    // english msg
+    message: "Too many accounts created from this IP, please try again after an hour"
+});
+
+router.post('/login', limiter ,AuthController.login)
 router.post('/register', AuthController.register)
 router.post('/forgot-password', AuthController.forgotPassword)
 
